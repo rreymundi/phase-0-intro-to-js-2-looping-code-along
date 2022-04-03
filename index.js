0c0e7fb4dcb8972f19f258ffa81f9695efe06745
raul reymundi <kinskispiral@gmail.com>
2022-04-03 17:16:01 -0400

    commit tree e912c8733271a8b943f9f255e4bd0753b7752160

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+function writeCards(names, event) {
+    const cards = [];
+    for (let i = 0; i < names.length; i++) {
+        cards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return cards;
+}
+
+//writeCards(["Guadalupe", "Ollie", "Aki"], "surprise");
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number--);
+    }
+}
